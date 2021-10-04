fa71da6e74294b5af69c4915bc11e0716156bf28
Akanksha6339 <90215583+Akanksha6339@users.noreply.github.com>
2021-10-04 20:21:52 +0530

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PriceComponent } from './price/price.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PriceComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
